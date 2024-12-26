75839091d4e958f1afcb2ecdce7f51de5c57d970
shakunage <veikka.junttila1@gmail.com>
2024-12-26 11:58:43 +0200

    add ex 13.1

---
+++ commands.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS blogs (
+    id SERIAL PRIMARY KEY NOT NULL,
+    author text NOT NULL,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes INTEGER DEFAULT 0
+);
+
+insert into blogs (author, url, title) values ('Steve Jobs', 'apple.com', 'Thoughts on Flash');
+insert into blogs (author, url, title) values ('Edsger Dijkstra', 'en.wikipedia.org/wiki/Considered_harmful', 'Go To Statement Considered Harmful');
