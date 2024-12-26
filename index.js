30fc9a3e7dd4300badad6402bde5bb4e872b206f
shakunage <veikka.junttila1@gmail.com>
2024-12-26 11:58:21 +0200

    [add] ex 13.1-13.4

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -12,40 +14,75 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-class Note extends Model {}
-Note.init({
+class Blog extends Model {}
+Blog.init({
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  content: {
+  author: {
     type: DataTypes.TEXT,
     allowNull: false
   },
-  important: {
-    type: DataTypes.BOOLEAN
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
   },
-  date: {
-    type: DataTypes.DATE
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   }
 }, {
   sequelize,
   underscored: true,
   timestamps: false,
-  modelName: 'note'
+  modelName: 'blog'
 })
 
 
-app.get('/api/notes', async (req, res) => {
-    const notes = await Note.findAll()
-    res.json(notes)
+const sqlFilePath = path.join(__dirname, 'commands.sql')
+const sqlCommands = fs.readFileSync(sqlFilePath, 'utf8')
+
+sequelize.query(sqlCommands)
+  .then(() => {
+    console.log('SQL commands executed successfully')
+  })
+  .catch(err => {
+    console.error('Error executing SQL commands:', err)
+  })
+
+app.get('/api/blogs', async (req, res) => {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
 })
 
-app.post('/api/notes', async (req, res) => {
+app.post('/api/blogs', async (req, res) => {
     try {
-      const note = await Note.create(req.body)
-      return res.json(note)
+      const blog = await Blog.create(req.body)
+      return res.json(blog)
+    } catch(error) {
+      return res.status(400).json({ error })
+    }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  console.log("req params id is: ", req.params.id)
+    try {
+      const result = await Blog.destroy({
+        where: {
+          id: req.params.id
+        },
+      });
+      if (result) {
+        return res.status(200).json({ message: 'Blog deleted successfully' });
+      } else {
+        return res.status(404).json({ error: 'Blog not found' });
+      }
     } catch(error) {
       return res.status(400).json({ error })
     }
@@ -55,3 +92,12 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const logBlogs = async () => {
+  const blogs = await Blog.findAll()
+  for (const blog of blogs) {
+    console.log(`${blog.author}: ${blog.title}, ${blog.url}, ${blog.likes} likes`)
+  }
+}
+
+logBlogs()
