825c8b917de4119c26b1105d084da4359e5d5c91
shakunage <veikka.junttila1@gmail.com>
2025-01-20 20:37:25 +0900

    ex 13.23

--- controllers/tokenExtractor.js
+++ controllers/tokenExtractor.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const tokenExtractor = (req, res, next) => {
     const authorization = req.get('authorization')
-    console.log(authorization)
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
       try {
         req.decodedToken = jwt.verify(authorization.substring(7), "your_secret_key")
+        console.log(req.decodedToken.id)
       } catch{
         return res.status(401).json({ error: 'token invalid' })
       }
